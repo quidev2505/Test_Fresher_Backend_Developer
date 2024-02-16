918d7635709736ebc2875c6dc8e07f4075b31c25
--replace-all <quidev2505@gmail.com>
2024-02-16 17:47:41 +0700

    finish answer all question of test

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Backend Take Home Assignment",
+  description: "Answer all question in home assignment",
+};
+
+export function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
